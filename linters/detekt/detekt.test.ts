bd7b3c1c975d2a218d11cc4aa89092900ff75e00
Tyler Jang <tyler@trunk.io>
2023-01-18 16:52:06 -0800

    asfdafsd

--- linters/detekt/detekt.test.ts
+++ linters/detekt/detekt.test.ts
@@ -6,9 +6,9 @@ import { TrunkDriver } from "tests/driver";
 import { TEST_DATA } from "tests/utils";
 
 // Running check on the input manually requires the existence of a top level .detekt.yaml
-        const preCheck = (driver: TrunkDriver) => {
-          driver.writeFile(".detekt.yaml", "");
-        };
+            const preCheck = (driver: TrunkDriver) => {
+              driver.writeFile(".detekt.yaml", "");
+            };
 
 // TODO(Tyler): We will eventually need to add a couple more test cases involving failure modes.
 linterCheckTest({ linterName: "detekt", namedTestPrefixes: ["basic_detekt"], preCheck });
